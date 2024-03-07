293d9ce144331e6800db1c642914d505ec6e53a1
Andrew Rush <rushndr@amazon.com>
2024-03-07 12:32:37 -0500

    Added neo-tree.nvim

--- init.lua
+++ init.lua
@@ -167,6 +167,20 @@ require('lazy').setup {
   --     vim.cmd.colorscheme 'solarized'
   --   end,
   -- },
+  --
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      vim.keymap.set('n', '<C-e>', ':Neotree filesystem reveal right toggle<CR>')
+    end,
+  },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
